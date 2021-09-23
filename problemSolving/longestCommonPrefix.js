68e6d5507eb7d6e35949a9d84031d84abe195fd5
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-09-23 12:33:05 -0400

    Longest common prefix complete

--- problemSolving/longestCommonPrefix.js
+++ problemSolving/longestCommonPrefix.js
@@ -1,34 +1,16 @@
 var longestCommonPrefix = function(strs) {
-    let commonPrefix = ""; 
-    let pointer = 0; 
-    let minStr = Math.min(strs[0].length, strs[1].length); 
-    while(pointer < minStr){
-        if(strs[0][pointer] === strs[1][pointer]){
-            commonPrefix += strs[0][pointer]
+    let commonPrefix = strs[0]; 
+    
+    for(let i = 1; i < strs.length; i++){
+        if(commonPrefix.length > strs[i].length){
+            commonPrefix = commonPrefix.substring(0, strs[i].length)
         }
-        pointer++;
-    }
-    
-    strs.splice(0, 2);
-    pointer = 0; 
-    
-    
-    console.log(commonPrefix)
-    if(commonPrefix.length !== 0){
-        let counter = 0; 
-        let currentWord = strs[counter];
-        while(pointer < currentWord.length && counter < strs.length){
-            if(pointer <= commonPrefix.length){
-                if(commonPrefix[pointer] !== strs[counter][pointer]){
-                    commonPrefix = commonPrefix.substring(0, commonPrefix.length - 1);
-                }
-                pointer++; 
-            } else {
-                pointer = 0; 
-                counter++; 
+        for(let j = 0; j < strs[i].length; j++){
+            if(commonPrefix[j] !== strs[i][j]){
+                commonPrefix = commonPrefix.substring(0, j); 
             }
         }
     }
     
-    return commonPrefix 
+    return commonPrefix; 
 };
